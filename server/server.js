5fe3276e612ed7caa3aeecdfdbfe3e4f979eaea9
อภิสิทธิ์ แป้นแหลม <aphisitpaenlaem2004@gmail.com>
2026-07-21 23:11:07 +0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const { UnoGame } = require('./game');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, {
+  cors: {
+    origin: 'https://unobyblackbara.netlify.app', // ใส่ URL จริงจาก Netlify
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(express.static('../public'));
 
